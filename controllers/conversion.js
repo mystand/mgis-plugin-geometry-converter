83d8427cbbce458b16b4c195fa9a1578ad0f2ae8
Kirill Klochkov <recound5@gmail.com>
2016-12-15 19:46:56 +0500

    end with conversions

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -6,11 +6,29 @@ import turf from '@turf/turf'
 import { selectFeatures, selectFeatureById } from 'core/shared/utils/utils'
 import Feature from 'core/backend/models/feature'
 
-const PICK_HANDLER = {
-  'polygon->point': PolygonToPoint,
-  'point->polygon': PointToPolygon,
-  'point->line': PointToLineString
+const ACTUAL_TYPES = {
+  polygon: 'Polygon',
+  line: 'LineString',
+  point: 'Point'
 }
+const PICK_HANDLER = {
+  'polygon->point': feature => turf.centerOfMass(feature),
+  'point->polygon': feature => turf.circle(turf.point(feature.geometry.coordinates), 10, 8, 'meters'),
+  'point->line': feature => R.pipe(
+    R.updatePath(['geometry', 'type'], () => 'LineString'),
+    R.updatePath(['geometry', 'coordinates'], x => [x[0][1], x[0][5]])
+    )(turf.circle(turf.point(feature.geometry.coordinates), 10, 8, 'meters')),
+  'line->point': feature => turf.along(feature, 0, 'meters'),
+  'polygon->line': feature => R.pipe(
+    R.updatePath(['geometry', 'type'], () => 'LineString'),
+    R.updatePath(['geometry', 'coordinates'], x => R.init(x[0]))
+    )(feature),
+  'line->polygon': feature => turf.envelope({
+    type: 'FeatureCollection',
+    features: [feature]
+  })
+}
+
 export async function index(ctx) {
   ctx.body = await ctx.knex(TABLE_NAME)
 }
@@ -28,52 +46,22 @@ export async function create(ctx) {
   .from('features')
   .where(ctx.knex.raw(`properties->>'layer_key' = '${layer_key}'`))
 
-  const features = featuresAsData.map((feature) => {
-    return {
-      type: 'Feature',
-      id: feature.id,
-      properties: feature.properties,
-      geometry: JSON.parse(feature.geometry)
-    }
+  const features = featuresAsData.map(feature => ({
+    type: 'Feature',
+    id: feature.id,
+    properties: feature.properties,
+    geometry: JSON.parse(feature.geometry)
   })
-  const updatedFeatures = await PICK_HANDLER[`${prevGeometryType}->${geometry_type}`](ctx, features, layer_key, prevGeometryType)
+  )
+  const updatedFeatures = R.pipe(
+    R.filter(x => x.geometry.type === ACTUAL_TYPES[prevGeometryType]),
+    R.map(feature => R.assoc('geometry',
+      PICK_HANDLER[`${prevGeometryType}->${geometry_type}`](feature).geometry,
+      feature))
+    )(features)
+
   await Feature.updateAll(ctx.knex, updatedFeatures)
   ctx.body = { success: true }
-}
-
-async function PolygonToPoint(ctx, features) {
-  const updatedFeatures = R.pipe(
-    R.filter(x => x.geometry.type === 'Polygon'),
-    R.map(feature => R.pipe(
-      R.assoc('properties', feature.properties),
-      R.assoc('id', feature.id)
-    )(turf.centerOfMass(feature)))
-    )(features)
-  return updatedFeatures
-}
-
-async function PointToPolygon(ctx, features) {
-  const updatedFeatures = R.pipe(
-    R.filter(x => x.geometry.type === 'Point'),
-    R.map(feature => R.pipe(
-      R.assoc('id', feature.id),
-      R.assoc('properties', feature.properties)
-      )(turf.circle(turf.point(feature.geometry.coordinates), 10, 8, 'meters')))
-    )(features)
-  return updatedFeatures
-}
-
-async function PointToLineString(ctx, features) {
-  const updatedFeatures = R.pipe(
-    R.filter(x => x.geometry.type === 'Point'),
-    R.map(feature => R.pipe(
-      R.updatePath(['geometry', 'type'], () => 'LineString'),
-      R.updatePath(['geometry', 'coordinates'], x => [x[0][0], x[0][2]]),
-      R.assoc('id', feature.id),
-      R.assoc('properties', feature.properties)
-      )(turf.circle(turf.point(feature.geometry.coordinates), 10, 4, 'meters')))
-    )(features)
-  return updatedFeatures
 }
 
 function error(ctx, message) {
